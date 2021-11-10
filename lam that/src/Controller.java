68b8d7d9bb2c9865d48209438bd8b41b094008e5
zxmclzxmcl <91659267+zxmclzxmcl@users.noreply.github.com>
2021-11-10 17:17:31 +0700

    Add files via upload

---
+++ lam that/src/Controller.java
@@ -0,0 +1,68 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class Controller implements Initializable{
+    @FXML
+    private TableView<Student> table;
+
+    @FXML
+    private TableColumn<Student, Integer> idColumn;
+
+    @FXML
+    private TableColumn<Student, String> nameColumn;
+
+    @FXML
+    private TableColumn<Student, String> emailColumn;
+
+    @FXML
+    private TableColumn<Student, Integer> ageColumn;
+
+    private ObservableList<Student> studentList;
+
+    @FXML
+    private TextField idText;
+
+    @FXML
+    private TextField nameText;
+
+    @FXML
+    private TextField emailText;
+
+    @FXML
+    private TextField ageText;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        studentList = FXCollections.observableArrayList(
+                new Student( 1,"Chau","Chau@gmail.com",21),
+                new Student( 2,"Chuong","Chuong@gmail.com",30)
+        );
+        idColumn.setCellValueFactory(new PropertyValueFactory<Student, Integer>("id"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<Student, String>("name"));
+        emailColumn.setCellValueFactory(new PropertyValueFactory<Student, String>("email"));
+        ageColumn.setCellValueFactory(new PropertyValueFactory<Student, Integer>("age"));
+        table.setItems(studentList);    
+    }
+    public void add (ActionEvent e){
+        Student newStudent = new Student();
+        newStudent.setId(Integer.parseInt(idText.getText()));
+        newStudent.setName(nameText.getText());
+        newStudent.setEmail(emailText.getText());
+        newStudent.setAge(Integer.parseInt(ageText.getText()));
+        studentList.add(newStudent);
+    }
+    public void delete (ActionEvent e){
+        Student selected = table.getSelectionModel().getSelectedItem();
+        studentList.remove(selected);
+    }
+}
